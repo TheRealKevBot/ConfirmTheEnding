306fb7e6a3900e305bf76e2e6a16fd73f418fe0a
TheRealKevBot <Kglas001@gmail.com>
2021-05-26 19:13:07 -0600

    Build

--- index.js
+++ index.js
@@ -2,10 +2,34 @@
 // This challenge can be solved with the .endsWith() method, which was introduced in ES2015. 
 // But for the purpose of this challenge, we would like you to use one of the JavaScript substring methods instead. 
 
+// function confirmEnding(str, target) {
+//     if (str.endsWith(target)) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
 // Should return true or false
 
 function confirmEnding(str, target) {
-    return str;
-  }
+    let substring = str.substring(str.length - target.length, str.length)
+    console.log(substring)
+    console.log(target)
+    if(substring === target) {
+        return true
+    } else {
+        return false
+    }
+}
   
-  confirmEnding("Bastian", "n");
+console.log(confirmEnding("Bastian", "n"));
+console.log(confirmEnding("Congratulation", "on"));
+console.log(confirmEnding("Connor", "n"));
+console.log(confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification"));
+console.log(confirmEnding("He has to give me a new name", "name"));
+console.log(confirmEnding("Open sesame", "same"));
+console.log(confirmEnding("Open sesame", "sage"));
+console.log(confirmEnding("Open sesame", "game"));
+console.log(confirmEnding("If you want to save our world, you must hurry. We dont know how much longer we can withstand the nothing", "mountain"));
+console.log(confirmEnding("Abstraction", "action"));
